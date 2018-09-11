badbf24a49f65a351b5ab18f8f2d4e4d6590dab5
Milesq <wmilosz88@gmail.com>
2018-09-11 17:58:33 +0200

    Settings page

---
+++ scripts/settings.js
@@ -0,0 +1,15 @@
+function afterEvent () {
+    let date = document.getElementById('dateInput').value,
+        phones = document.getElementById('phonesInput').value;
+    chrome.storage.local.set({data: {
+        date: date,
+        phones: phones
+    }});
+}
+
+document.getElementById('saveData').addEventListener('click', afterEvent);
+chrome.storage.local.get(['data'], response => {
+    console.log(response);
+    document.getElementById('phonesInput').value = response.data.phones;
+    document.getElementById('dateInput').value = response.data.date
+});
